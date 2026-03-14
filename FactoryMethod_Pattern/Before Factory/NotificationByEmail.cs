31f1632c399c994ec726300e4727aa7ffd1839f7
Yosif Taha <ywsfwhab314@gmail.com>
2026-03-14 23:15:21 +0200

    Implement Factory Method pattern with a simple C# example

---
+++ FactoryMethod_Pattern/Before Factory/NotificationByEmail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod_Pattern.Before_Factory
+{
+    public class NotificationByEmail
+    {
+        public void Send(string user, string message)
+        {
+            Console.WriteLine($"Sending Email notification By {user}: {message}");
+        }
+    }
+}
---
+++ FactoryMethod_Pattern/Program.cs
@@ -0,0 +1,40 @@
+﻿using FactoryMethod_Pattern.After_Factory.FactoryMethod;
+
+namespace FactoryMethod_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter type (sms/email) :");
+            var type = Console.ReadLine();
+
+            Console.WriteLine("Please Enter Name of user :");
+            var user = Console.ReadLine();
+
+            Console.WriteLine("Please Enter Message :");
+            var message = Console.ReadLine();
+
+
+            #region Before Factory
+            //if (type.ToLower() == "sms")
+            //{
+            //    var notification = new NotificationBySms();
+            //    notification.Send(uder, message);
+            //}
+            //else if (type.ToLower() == "email")
+            //{
+            //    var notification = new NotificationByEmail();
+            //    notification.Send(uder, message);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Please Enter The Type");
+            //} 
+            #endregion
+
+            // After Factory
+            NotificationFactory.CreateNotification(type).Send(user, message);
+        }
+    }
+}
