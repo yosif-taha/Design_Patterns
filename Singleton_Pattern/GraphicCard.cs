59c12f03686083da12ed64d1141d1ed5951d2190
Yosif Taha <ywsfwhab314@gmail.com>
2026-03-14 23:08:44 +0200

    Add a simple implementation of the Singleton Design Pattern in C#

---
+++ Singleton_Pattern/GraphicCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern
+{
+    internal class GraphicCard
+    {
+        public int Data { get; set; }
+
+        // 1. Constructor Must be Private
+        private GraphicCard(int data)
+        {
+            Data = data;
+        }
+
+        public static GraphicCard Singleton = null!;
+
+        // 2.1 Static Prop to create instance of the class
+
+        public static GraphicCard SingletonObj { get; } = new GraphicCard(123);
+
+
+        // 2.1 Static Method to create instance of the class
+        static GraphicCard()
+        {
+            Singleton = new GraphicCard(512);
+        }
+        public static GraphicCard GetCard()
+        {            
+            return Singleton;
+        }
+    }
+}
