4ee0551bb15196e06058d599c61b65a67ab52aa9
Yosif Taha <ywsfwhab314@gmail.com>
2026-03-19 02:27:54 +0200

    An example demonstrating the Decorator Pattern by wrapping objects to extend their functionality in a flexible and reusable way.

---
+++ Decorator_Pattern/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Decorator_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Drink Espresso = new Espresso();
+            Console.WriteLine(Espresso);
+
+            Milk EspressoWithMilk = new Milk(Espresso);
+            Console.WriteLine(EspressoWithMilk);
+
+            Mocha EspressoWithMilkAndMocha = new Mocha(EspressoWithMilk);
+            Console.WriteLine(EspressoWithMilkAndMocha);
+        }
+    }
+}
